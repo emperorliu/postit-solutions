a7da799fc4274a658c8c14a42d782febc0102319
emperorliu <mangomystery@gmail.com>
2014-09-23 12:44:04 -0700

    changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    binding.pry
     @post = Post.find(params[:post_id])
     @comment = @post.comments.build(params.require(:comment).permit(:body))
     @comment.creator = User.first
